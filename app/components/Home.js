cdd88491700f9c792946e346ff2235c33d459b62
Zachary Lee <w.zachary.lee@gmail.com>
2016-05-14 23:56:22 -0500

    in the middle of implementing audio player

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Header from './Header'
-import Episode1 from './Episode1'
+import Episodes from './Episodes'
 
 var App = React.createClass({
+  getInitialState: function() {
+    //state is null originally, until page loads
+    return {
+      currentEpisode: null
+    }
+  },
+  changeEpisode: function(newEpisode) {
+    this.setState({currentEpisode: newEpisode})    
+  },
+  componentDidMount: function() {
+    //since the page is loaded, we set the state based on the url params
+    if (window.location.search.length !== 0) {
+      setState({currentEpisode: window.location.search})
+    }
+  },
   render: function() {
     return (
       <div>
         <Header />
-        <Episode1 />
+        <Episodes 
+          currentEpisode={this.state.currentEpisode }
+          changeEpisode={this.changeEpisode}
+        />
       </div>
     );
   }
