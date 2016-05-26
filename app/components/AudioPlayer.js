e2e8ef1f91d288f212abd2b6999210bdb7cef425
Nayigiziki <josephnayigiziki@gmail.com>
2016-05-26 09:13:56 -0500

    updated audio player styling

--- app/components/AudioPlayer.js
+++ app/components/AudioPlayer.js
@@ -9,8 +9,6 @@ var AudioPlayer = React.createClass({
       trackNumber: ""
     }
   },
-  componentWillMount: function() {
-  },
   componentWillReceiveProps: function(nextProps) {
     var songUrl = songs[nextProps["currentEpisode"]] ? songs[nextProps["currentEpisode"]]["songPath"] : '';
     var songTitle = songs[nextProps["currentEpisode"]]? songs[nextProps["currentEpisode"]]["songTitle"] : '';
@@ -21,12 +19,33 @@ var AudioPlayer = React.createClass({
     }
   },
   playNewSong: function() {
-    var audioPlayer = document.getElementById('audioPlayer')
+    var audioPlayer = document.getElementById('audioPlayer');
     audioPlayer.play();
+  },
+  audioPlayerHandler: function(action){
+    var audioPlayer = document.getElementById('player');
+
+    switch(action){
+      case 'play':
+        audioPlayer.play();
+        break;
+      case 'pause': 
+        audioPlayer.pause();
+        break;
+      case 'stop':
+        audioPlayer.stop();
+        break;
+      default:
+        break;
+    }
   },
   render: function() {
     var titleElem = null;
     var idString = 'hide';
+    var playHandler = this.audioPlayerHandler.bind(null, 'play');
+    var pauseHandler = this.audioPlayerHandler.bind(null, 'pause');
+    var stopHandler = this.audioPlayerHandler.bind(null, 'stop');
+
     if(this.state.currentSongUrl){
       var title = this.state.currentSongTitle;
       var trackNumber = this.state.trackNumber;
@@ -38,7 +57,14 @@ var AudioPlayer = React.createClass({
     return (
       <div id={idString}>
         <span className='songTrackNumberContainer'>{trackNumberElem}</span>
-        <span className='songTitleContainer'>{titleElem}</span>
+        <span className='songTitleContainer'>
+          <span className='titleElem'>{titleElem}</span>
+          <span className='playerElemsContainer'>
+            <span className='playerElems' onClick={playHandler}>Play</span>
+            <span className='playerElems' onClick={pauseHandler}>Pause</span>
+            <span className='playerElems'onClick={stopHandler} >Stop</span>
+          </span>
+        </span>
         <audio id='player' src={this.state.currentSongUrl} controls></audio>
       </div>
     );
